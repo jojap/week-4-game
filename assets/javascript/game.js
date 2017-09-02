109b15f32bbed31e7556212734d3bd0b89721bdd
josh.pierce <jepierce1992@gmail.com>
2017-09-01 22:01:25 -0400

    combined my earlier work into this project, creating a functioning game with major styling needed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
     function start() {
         $("#compNumber").empty();
         $("#userNumber").html(0);
-        userScore = 0;
+        currentScore = 0;
         targetNumber = Math.floor(Math.random() * (120 - 19) + 19);
         $("#compNumber").html(targetNumber);
-        console.log(targetNumber)
+        console.log(targetNumber);
 
 
 
@@ -53,6 +53,46 @@ $(document).ready(function () {
 
         greenGem.attr("data-gemValue", greenValue);
     }
+
+    $(".crystal-image").on("click", function () {
+
+        //targets the clicked gem picture and retrieves its stored value (random number between 1 and 12)
+        var crystalValue = ($(this).attr("data-gemValue"));
+
+        //the returned string value of the random number is converted to an integer
+        crystalValue = parseInt(crystalValue);
+
+        //for each click, add to a global currentScore used for the player's score
+        currentScore += crystalValue;
+
+        //updates the player's current score on the page
+        $("#userNumber").text(currentScore);
+
+        //win condition, user wins when their score equals the target number
+        if (targetNumber === currentScore) {
+            //updates page to say the user won
+            $("#win-lose").html("<h4>You won!</h4>").attr("class", "green-font");
+            //win count is increased by 1
+            wins += 1;
+            //win count is updated on the page
+            $("#wins").text(wins);
+            //runs a reset function
+            start();
+        }
+        //lose condition, user loses if their score is greater than the target number
+        else if (targetNumber < currentScore) {
+            //updates page to say user lost
+            $("#win-lose").html("<h4>You lost!</h4>").attr("class", "red-font");
+            //loss count is increased by 1
+            losses += 1;
+            //loss count is updated on the page
+            $("#losses").text(losses);
+            //runs a reset function
+            start();
+        }
+
+    })
+
 
 
 
